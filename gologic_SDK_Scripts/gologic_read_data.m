3ba96c74929e592856a139c0172744d266fd8891
Jorgedltm <jorgedlt001@gmail.com>
2025-07-24 17:23:24 +0200

    GoLogic matlab scripts & SDK

---
+++ gologic_SDK_Scripts/gologic_read_data.m
@@ -0,0 +1,45 @@
+function [alpha, clk, data_HDR, data_HSNR] = gologic_read_data(manualMode, pdmMode, DDR_mode, clkTriggered, varargin)
+    
+    p = inputParser;
+    addParameter(p, 'saveFileName', 'default_data_export.csv');
+    addParameter(p, 'saveDirectory', '');
+    parse(p, varargin{:});
+   
+    if manualMode && ~(any(char(p.Results.saveDirectory)) && any(char(p.Results.saveFileName)))
+        error("In manual capture mode, the file path needs to be specified. " + newline + ...
+            "Please, input saveDirectory and saveFileName to the funtion.")
+    end
+
+    if ~manualMode
+
+        fullPath = mfilename('fullpath');
+        functionDir = fileparts(fullPath);
+        setDir = strcat(functionDir,"\set_files\");
+        
+        if pdmMode
+            if clkTriggered
+                setFile = strcat(setDir,"config_pdm.set");
+            else
+                setFile = strcat(setDir,"config_pdm_ntrig.set");
+            end
+        else
+            if clkTriggered
+                setFile = strcat(setDir,"config4m.set");
+            else
+                setFile = strcat(setDir,"config_npdm_ntrig.set");
+            end
+        end
+    
+        if isfile(setFile)
+            disp("<strong>Starting data acquisition...</strong>");
+        else
+            error("Set file couldn't be found, please check file location is at %s", setFile);
+        end
+        
+        %To capture the 13 signals from a12-a00, and additional port has to be
+        %defined (a13-a00), because of SDK malfunction.
+       
+        [status, cmdout] = system([functionDir, '\SDKCmdUtility.exe s="', char(setFile), '" o="',char(p.Results.saveDirectory), char(p.Results.saveFileName),'" u=o c="a00-a13" b=BIN #=n k=n']);
+    end
+    [alpha, clk, data_HDR, data_HSNR] = import_gologic_DOG15([char(p.Results.saveDirectory), char(p.Results.saveFileName)],DDR_mode);
+end
