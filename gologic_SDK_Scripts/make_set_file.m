9ba78491a1350cf3107a33261960d19e9994b74a
Jorgedltm <jorgedlt001@gmail.com>
2025-07-26 16:05:10 +0200

    Set file automation.

---
+++ gologic_SDK_Scripts/make_set_file.m
@@ -0,0 +1,27 @@
+function outFile = make_set_file(samplingMode,samplingFreq,clkEdgeMode)
+
+    fullPath = mfilename('fullpath');
+    functionDir = fileparts(fullPath);
+    
+    inputFile = strcat(functionDir, "\set_files\config_master.set");
+    outFile = strcat(functionDir, "\set_files\config_custom.set");
+
+    if (samplingFreq == "clk triggered")
+        samplingFreq = "500MHz";
+    end
+
+    if (samplingMode == "simple state" || samplingMode == "normal timing") && (samplingFreq == "100MHz" || samplingFreq == "500MHz") && (clkEdgeMode == "Falling" || clkEdgeMode == "Any" || clkEdgeMode == "Rising")
+
+        txt = fileread(inputFile);
+        txt_modificado = strrep(txt, '%sampling_mode%', samplingMode);
+        txt_modificado = strrep(txt_modificado, '%sampling_freq%', samplingFreq);
+        txt_modificado = strrep(txt_modificado, '%clk_mode%', clkEdgeMode);
+
+        fid = fopen(outFile, 'w');
+        fwrite(fid, txt_modificado);
+        fclose(fid);
+    else
+        error("Invalid parameters: Check sampling mode, frequency, and clock edge mode." + newline + "Sampling mode: simple state (clk triggered) or normal timing." + newline + "Sampling freq: 100MHz or clk triggered" ...
+            + newline + "Clock edge mode: Falling or Rising or Any");
+    end
+end
